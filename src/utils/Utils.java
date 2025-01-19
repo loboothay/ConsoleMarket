8d6145031fd98d8d4e1b41e297afd84870f22328
Thaynara Lobo <loboo.thaynara@gmail.com>
2025-01-18 21:28:01 -0300

    feat: adicionado os pacotes e incrementado a classe Produto e ajuste de método da utils.

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 public class Utils {
     static NumberFormat formatter = new DecimalFormat("R$ #,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
 
-    public static String doubleToString(String value) {
+    public static String doubleToString(Double value) {
         return formatter.format(value);
     }
 }
