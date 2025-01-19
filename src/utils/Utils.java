74bbdd6a68a338081ebfa38894254df266c76af0
Thaynara Lobo <loboo.thaynara@gmail.com>
2025-01-18 21:16:36 -0300

    feat: adicionado os pacotes e incrementado a classe utils para a formatação de moeda.

---
+++ src/utils/Utils.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Utils {
+    static NumberFormat formatter = new DecimalFormat("R$ #,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
+
+    public static String doubleToString(String value) {
+        return formatter.format(value);
+    }
+}
